939e89ff48815e0dd44eeaaaac1f1ecf4214195c
niigyanchristian <niigyanchristian@gmail.com>
2024-07-28 04:36:19 +0000

    version 0.0.1

--- src/api/products.js
+++ src/api/products.js
@@ -83,7 +83,9 @@ module.exports = (app,channel) => {
       const { data } = await service.GetShops(_id);
       return res.status(200).json(data);
     } catch (error) {
-      return res.status(404).json({ error });
+      console.log(error);
+      return res.status(200).json([]);
+      // return res.status(404).json({ error });
     }
   });
 
